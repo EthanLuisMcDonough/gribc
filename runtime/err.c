8554b5fe44beaea71f4d170b7b094a41a296b7f0
Ethan Luis McDonough <elmcdonough@comcast.net>
2019-06-17 20:04:01 -0500

    Basic error output

--- runtime/err.c
+++ runtime/err.c
@@ -1,11 +1,11 @@
 #include "err.h"
 #include <stdlib.h>
 
-void print_err() {
-    // @TODO
+void print_err(const char* msg) {
+    fprintf(stderr, "%s\n", msg);
 }
 
-void grib_panic() {
-    // @TODO
+void grib_panic(const char* msg) {
+    print_err(msg);
     exit(EXIT_FAILURE);
 }
--- runtime/err.h
+++ runtime/err.h
@@ -3,4 +3,7 @@
 
 #include <stdio.h>
 
+void grib_panic(const char* msg);
+void print_err(const char* msg);
+
 #endif
